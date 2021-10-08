a8454eca4d5464da232884ad9acb326e3792aa83
tastytim <tastytimgm@gmail.com>
2021-10-08 16:59:25 +0200

    deleted functions

--- app.js
+++ app.js
@@ -1,35 +1,13 @@
-let chilometri;
-let eta;
-let prezzoPerViaggio;
 
 
-function controlloChilometri() {
-  chilometri = prompt("Quanti chilometri vuoi percorrere ?");
-  if (chilometri === null || isNaN(chilometri)) {
-    alert("Hai inserito i dati sbagliati , riprova");
-    controlloChilometri();
-  }
-}
+let chilometri = prompt("Quanti chilometri vuoi percorrere ?");
+let eta = prompt("Inserisci la tua eta");
+let prezzoPerViaggio = chilometri * 0.21;
 
-function controlloEta() {
-  eta = prompt("Inserisci la tua eta");
-  if (eta === null || isNaN(eta) ||  eta < 1 || eta > 120 ) {
-    alert("Hai inserito i dati sbagliati , riprova");
-    controlloEta();
-  }
-}
-
-function calcoloPrezzoFinale() {
-  prezzoPerViaggio = chilometri * 0.21;
-
-  if (eta < 18) {
+if (eta < 18) {
     prezzoPerViaggio = prezzoPerViaggio - (prezzoPerViaggio * 0.2);
   } else if (eta >= 65) {
     prezzoPerViaggio = prezzoPerViaggio - (prezzoPerViaggio * 0.4);
   }
-  alert(prezzoPerViaggio.toFixed(2));
-}
 
-controlloChilometri();
-controlloEta();
-calcoloPrezzoFinale();
+alert(prezzoPerViaggio.toFixed(2));
